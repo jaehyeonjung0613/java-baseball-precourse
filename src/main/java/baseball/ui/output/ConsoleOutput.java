5c65016e3d7b72347bba17974da57a38f04ce22b
jaehyeonjung0613 <jaehyeonjung0613@gmail.com>
2024-12-20 16:38:24 +0900

    test, feat: 15. 개행 출력 구분 - [ConsoleOutput.java] 콘솔 개행 출력 여부에 따른 함수 분리 - [Output.java] 개행 출력 인터페이스 추가

--- src/main/java/baseball/ui/output/ConsoleOutput.java
+++ src/main/java/baseball/ui/output/ConsoleOutput.java
@@ -3,6 +3,11 @@ package baseball.ui.output;
 public class ConsoleOutput implements Output {
 	@Override
 	public void print(String message) {
+		System.out.print(message);
+	}
+
+	@Override
+	public void println(String message) {
 		System.out.println(message);
 	}
 
--- src/main/java/baseball/ui/output/Output.java
+++ src/main/java/baseball/ui/output/Output.java
@@ -3,5 +3,7 @@ package baseball.ui.output;
 public interface Output {
 	void print(String message);
 
+	void println(String message);
+
 	void printByFormat(String format, Object... args);
 }
